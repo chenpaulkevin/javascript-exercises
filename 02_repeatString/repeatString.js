ac991ed1aad4fac1b27e0aea3df4fd41c9134572
chenpaulkevin <chenpaulkevin@gmail.comssh-keygen -t rsa -b 4096 -C chenpaulkevin@gmail.com>
2023-08-12 20:55:27 +0800

    Clean and optimize code for repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,15 @@
-const repeatString = function(givenString, repitions) {
-    let concatenatedString = "";
-    if (repitions >= 0) {
-        for (let index = 0; index < repitions; index++) {
-            concatenatedString = concatenatedString.concat(givenString);
-        }
-        return concatenatedString;
+const repeatString = (givenString, repetitions) => {
+    if (repetitions >= 0) {
+      let result = "";
+      
+      for (let index = 0; index < repetitions; index++) {
+        result += givenString;
+      }
+      
+      return result;
+    } else {
+      return "ERROR";
     }
-    else{
-        return 'ERROR';
-    }
-
-};
-
+  };
 // Do not edit below this line
 module.exports = repeatString;
